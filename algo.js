a826aca2b520c92f9ce70059e5897d8b5f2f782b
Sam_Osho <Sosho108@marian.edu>
2024-06-21 21:58:00 -0400

    Simple algo challenges in js!

---
+++ algo.js
@@ -0,0 +1,57 @@
+// Set myNumber to 42. Set myName to your name. Now swap myNumber into myName & vice versa.
+
+
+// myNumber = 42 
+// myName = "sam"
+
+
+// tempName = myName
+// tempNum = myNumber
+// myName =tempNum
+// myNumber = tempName
+
+
+// console.log(myNumber)
+// console.log(myName)
+
+// Log positive numbers starting at 2016, counting down by fours (exclude 0), without a FOR loop.
+
+bigNum = 2016
+
+divider = 4
+
+num = 0
+
+array = []
+
+end = 0
+
+
+while (bigNum > end){
+
+    bigNum = bigNum - 4 
+    num = bigNum
+    array.push(num)
+}
+console.log(array)
+
+
+
+// division = (x,y) =>{
+
+
+//     let j = 0
+//     if (j < x ){
+
+//         num = x/y
+//         console.log(num)
+//     }
+//     // x - 4 
+//     // num =  j - y
+//     // console.log(num) 
+    
+// }
+
+// division(2016,4)
+
+
